e8543c383e1db337e7fc72282457b52984094d5a
Josedlp94 <j.delapuenterizopatron@gmail.com>
2026-04-23 12:44:28 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tmf-v1.0.5';
+const CACHE = 'tmf-v2.0.0';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
  
 self.addEventListener('install', e => {
